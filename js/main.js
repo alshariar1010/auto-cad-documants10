ac3c991beaeb14f47ced86542443fba5c37b4a54
MD.AL-SHARIAR SHAIKH <alshariar1010@gmail.com>
2026-07-14 10:03:01 +0600

    one section complate

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+    confetti({
+        particleCount:150,
+        spread:90,
+        origin:{y:0.1},
+        colors:['#FF0000','#0000FF','#00FF00','#FFFF00']
+    });
+};
